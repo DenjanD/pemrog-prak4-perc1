10cf85a2bba4c14f237cf223f443d7f44bca439e
DenjanD <alfien311@gmail.com>
2021-11-26 23:09:15 +0700

    first commit

---
+++ P4_1_1204039/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace P4_1_1204039
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.vbButton = new System.Windows.Forms.Button();
+            this.csButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(32, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(486, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Which do you think is better, Visual Basic or C#?";
+            // 
+            // vbButton
+            // 
+            this.vbButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.vbButton.ForeColor = System.Drawing.Color.Green;
+            this.vbButton.Location = new System.Drawing.Point(114, 74);
+            this.vbButton.Name = "vbButton";
+            this.vbButton.Size = new System.Drawing.Size(100, 23);
+            this.vbButton.TabIndex = 1;
+            this.vbButton.Text = "Visual Basic";
+            this.vbButton.UseVisualStyleBackColor = true;
+            this.vbButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.vbButton_MouseMove);
+            // 
+            // csButton
+            // 
+            this.csButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.csButton.ForeColor = System.Drawing.Color.Red;
+            this.csButton.Location = new System.Drawing.Point(267, 74);
+            this.csButton.Name = "csButton";
+            this.csButton.Size = new System.Drawing.Size(100, 23);
+            this.csButton.TabIndex = 2;
+            this.csButton.Text = "C#";
+            this.csButton.UseVisualStyleBackColor = true;
+            this.csButton.Click += new System.EventHandler(this.csButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(32, 141);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 214);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.csButton);
+            this.Controls.Add(this.vbButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button vbButton;
+        private Button csButton;
+        private Button closeButton;
+    }
+}
